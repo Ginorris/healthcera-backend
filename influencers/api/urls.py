e68dd3601382ae58de2875d696fa111346963f16
Ginorris <rizzutigino@gmail.com>
2025-01-17 16:06:54 -0300

    fixed project structure

--- influencers/api/urls.py
+++
@@ -1,8 +0,0 @@
-from influencers.api.views import InfluencerRetrieveView, HomePageView
-from django.urls import path
-
-
-urls = [
-    path('influencer/<str:pk>/', InfluencerRetrieveView.as_view(), name='influencer-retrieve'),
-    path('', HomePageView.as_view(), name='home'),
-]
--- influencers/api/utils.py
+++
@@ -1,7 +0,0 @@
-from statistics import mean
-from models import Claim
-
-
-def get_claims_avg_score(claims: Claim):
-    """Given the claims of an influencer, calculate the trust score."""
-    return mean(claims.selet_related('score')).round()
